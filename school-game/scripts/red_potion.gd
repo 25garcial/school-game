b76697983bfa8733456561298db9cca6c82b387d
25garcial <25garcial@camraiders.com>
2024-11-23 21:22:38 -0600

    i did some things

--- school-game/scripts/red_potion.gd
+++ school-game/scripts/red_potion.gd
@@ -1,5 +1,7 @@
-extends RigidBody2D
-
+extends Area2D
+#function to throw a potion is in player script
+@onready var animated_sprite_2d: AnimatedSprite2D = $"../AnimatedSprite2D"
+@onready var sprite_2d: Sprite2D = $"../Sprite2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +13,11 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("enemy"):
-		$AnimatedSprite2D.play()
-	pass # Replace with function body.
+func _on_area_entered(area: Area2D) -> void: 	
+	if area.is_in_group("enemy"):
+		animated_sprite_2d.show()
+		sprite_2d.hide()
+		animated_sprite_2d.play()
+	
+func _on_animated_sprite_2d_animation_finished() -> void: #deletes itself when the animation finishes
+	$"..".queue_free()# Replace with function body.
