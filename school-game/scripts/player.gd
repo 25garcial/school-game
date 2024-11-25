49608bd38ef486accc035247ef1eee70af5dcb28
25garcial <91680716+25garcial@users.noreply.github.com>
2024-11-25 12:53:19 -0600

    Update player.gd with Tommy's code

--- school-game/scripts/player.gd
+++ school-game/scripts/player.gd
@@ -19,6 +19,11 @@ func throwPotion(potion):
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	top_down_movement(delta)
+	side_movement(delta)
+	move_and_slide()
+
+func side_movement(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -35,4 +40,61 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func top_down_movement(_delta):
+	
+	if Input.is_action_pressed("right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = -speed
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("moving_right")
+		elif movement == 0:
+			anim.play("idle")
+
+	if dir == "left":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("moving_left")
+		elif movement == 0:
+			anim.play("idle")
+
+	if dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("moving_forward")
+		elif movement == 0:
+			anim.play("idle")
+
+	if dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("moving_backward")
+		elif movement == 0:
+			anim.play("idle")
