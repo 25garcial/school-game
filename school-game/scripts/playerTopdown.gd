4e18fb8b1d15b4daedceada8d5f7d5756468acc1
IApologizeForMyName <25jenkinsj@camraiders.com>
2025-01-10 19:31:38 -0600

    fixed movement and collision

--- school-game/scripts/playerTopdown.gd
+++ school-game/scripts/playerTopdown.gd
@@ -8,58 +8,62 @@ func _physics_process(delta):
 	move_and_slide()
 
 func player_movement(_delta):
+	var move_input = Vector2.ZERO
 	
+	# Check input for horizontal and vertical movement
 	if Input.is_action_pressed("right"):
+		move_input.x += 1
 		current_dir = "right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left"):
+		move_input.x -= 1
 		current_dir = "left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("down"):
+	if Input.is_action_pressed("down"):
+		move_input.y += 1
 		current_dir = "down"
-		play_anim(1)
-		velocity.x = 0
-		velocity.y = speed
-	elif Input.is_action_pressed("up"):
+	if Input.is_action_pressed("up"):
+		move_input.y -= 1
 		current_dir = "up"
-		play_anim(1)
-		velocity.x = 0
-		velocity.y = -speed
-	else:
+
+	# Normalize the vector to ensure consistent speed in all directions (including diagonals)
+	if move_input.length() > 0:
+		move_input = move_input.normalized()
+
+	# Apply the velocity based on the input direction
+	velocity = move_input * speed
+
+	# If no input, stop and play idle animation
+	if move_input == Vector2.ZERO:
 		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
+
+	else:
+		play_anim(1)
 
 func play_anim(movement):
-	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
-	if dir == "right":
+	# Play the appropriate animation based on the direction
+	if current_dir == "right":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("moving_right")
 		elif movement == 0:
 			anim.play("idle")
 
-	if dir == "left":
-		anim.flip_h = false
+	elif current_dir == "left":
+		anim.flip_h = false  # Flip horizontally for left movement
 		if movement == 1:
 			anim.play("moving_left")
 		elif movement == 0:
 			anim.play("idle")
 
-	if dir == "down":
+	elif current_dir == "down":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("moving_forward")
 		elif movement == 0:
 			anim.play("idle")
 
-	if dir == "up":
+	elif current_dir == "up":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("moving_backward")
